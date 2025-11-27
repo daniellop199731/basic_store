6fa07a386346481a987638f72ffee2055672646d
Daniel Lopez <daniellop199731@gmail.com>
2025-11-26 21:51:11 -0500

    Cambios en service y controller de products
    Estandarizacion de mensajes en servie
    Se quita la respuesta bad request del controller, ya que por el consumo feign cliente, toda respuesta 400 o 500 se considera una excepcion

--- src/main/java/com/daniel/springcloud/msvc/products/application/useCase/ProductApplicationService.java
+++ src/main/java/com/daniel/springcloud/msvc/products/application/useCase/ProductApplicationService.java
@@ -19,23 +19,35 @@ public class ProductApplicationService implements ProductUseCase  {
 
     private final ProductRepositoryPort productRepository;
 
-    private ResponseGenericObject<List<Product>> productListObj = new ResponseGenericObject<>();
-    private ResponseGenericObject<Product> productObj = new ResponseGenericObject<>();
+    private ResponseGenericObject<List<Product>> productListObj     = new ResponseGenericObject<>();
+    private ResponseGenericObject<Product> productObj               = new ResponseGenericObject<>();
     
     private String message;
+
+    private static final String DEFAULT_ERROR_MESSAGE               = "Error en la ejecucion del proceso: ";
+    private static final String NOT_FOUND_PRODUCTS_MESSAGE          = "No se encontraron productos";
+    private static final String FOUND_PRODUCTS_MESSAGE              = "Productos encontrados exitosamente";    
+    private static final String FOUND_PRODUCT_MESSAGE               = "Producto encontrado";
+    private static final String NOT_FOUND_PRODUCT_BY_ID_MESSAGE     = "No se encontró el producto con ID: ";
+    private static final String PRODUCT_CREATED_SUCCESS_MESSAGE     = "Producto creado exitosamente";
+    private static final String PRODUCT_CREATED_NOT_SUCCESS         = "No se pudo crear el producto";
+    private static final String INVALID_PRODUCT_ID_MESSAGE          = "El ID del producto no es válido";
+    private static final String INVALID_PRODUCT_DATA_MESSAGE        = "Los datos del productos no son válidos";
+    private static final String PRODUCT_UPDATED_SUCCESS_MESSAGE     = "Producto actualizado exitosamente";
+    private static final String PRODUCT_UPDATED_NOT_SUCCESS         = "No se pudo actualizar el producto";
 
     @Override
     @Transactional(readOnly = true)
     public ResponseGenericObject<List<Product>> getAllProducts() {        
-        message = "No se encontraron productos";
+        message = NOT_FOUND_PRODUCTS_MESSAGE;
         try{
             List<Product> products = productRepository.findAll();
             if(!products.isEmpty()){
-                message = "Productos encontrados exitosamente";
+                message = FOUND_PRODUCTS_MESSAGE;
             }
             productListObj.setAsSuccessful(message, products);
         } catch (Exception e){
-            message = "Error en la ejecucion del proceso: " + e.getMessage();
+            message = DEFAULT_ERROR_MESSAGE + e.getMessage();
             productListObj.setAsWithErrors(message);
         }
         return productListObj;
@@ -44,17 +56,17 @@ public class ProductApplicationService implements ProductUseCase  {
     @Override
     @Transactional(readOnly = true)
     public ResponseGenericObject<Product> getProductById(Long id) {
-        message = "No se encontró el producto con ID: " + id;
+        message = NOT_FOUND_PRODUCT_BY_ID_MESSAGE + id;
         try{
             Optional<Product> product = productRepository.findById(id);
             if(!product.isPresent()){
                 productObj.setAsNotSuccessful(message);
                 return productObj;
             }
-            message = "Producto encontrado";
+            message = FOUND_PRODUCT_MESSAGE;
             productObj.setAsSuccessful(message, product.get());
         } catch (Exception e){
-            message = "Error en la ejecucion del proceso: " + e.getMessage();
+            message = DEFAULT_ERROR_MESSAGE + e.getMessage();
             productObj.setAsWithErrors(message);            
         }
         return productObj;
@@ -66,14 +78,14 @@ public class ProductApplicationService implements ProductUseCase  {
         try{
             Product savedProduct = productRepository.save(product);
             if(savedProduct.getId() != null){
-                message = "Producto creado exitosamente";
+                message = PRODUCT_CREATED_SUCCESS_MESSAGE;
                 productObj.setAsSuccessful(message, savedProduct);
             } else {
-                message = "No se pudo crear el producto";
+                message = PRODUCT_CREATED_NOT_SUCCESS;
                 productObj.setAsNotSuccessful(message);
             }
         } catch (Exception e){
-            message = "Error en la ejecucion del proceso: " + e.getMessage();
+            message = DEFAULT_ERROR_MESSAGE + e.getMessage();
             productObj.setAsWithErrors(message); 
         }
         return productObj;
@@ -84,19 +96,19 @@ public class ProductApplicationService implements ProductUseCase  {
     public ResponseGenericObject<Product> updateProduct(Long id, Product product) {
         try{
             if(id <= 0){
-                message = "El ID del producto no es válido";
+                message = INVALID_PRODUCT_ID_MESSAGE;
                 productObj.setAsNotSuccessful(message);
                 return productObj;
             }
 
             if(product == null){
-                message = "Los datos del productos no son válidos";
+                message = INVALID_PRODUCT_DATA_MESSAGE;
                 productObj.setAsNotSuccessful(message);
                 return productObj;                
             }
 
             if(!productRepository.existsById(id)){
-                message = "No se encontró el producto con ID: " + id;
+                message = NOT_FOUND_PRODUCT_BY_ID_MESSAGE + id;
                 productObj.setAsNotSuccessful(message);
                 return productObj;
             }
@@ -104,14 +116,14 @@ public class ProductApplicationService implements ProductUseCase  {
             product.setId(id);
             Product savedProduct = productRepository.save(product);
             if(savedProduct.getId() != null){
-                message = "Producto actualizado exitosamente";
+                message = PRODUCT_UPDATED_SUCCESS_MESSAGE;
                 productObj.setAsSuccessful(message, savedProduct);
             } else {
-                message = "No se pudo actualizar el producto";
+                message = PRODUCT_UPDATED_NOT_SUCCESS;
                 productObj.setAsNotSuccessful(message);
             }            
         } catch (Exception e){
-            message = "Error en la ejecucion del proceso: " + e.getMessage();
+            message = DEFAULT_ERROR_MESSAGE + e.getMessage();
             productObj.setAsWithErrors(message);            
         }
 
@@ -123,13 +135,13 @@ public class ProductApplicationService implements ProductUseCase  {
     public ResponseGenericObject<Product> deleteProduct(Long id) {
         try{
             if(id <= 0){
-                message = "El ID del producto no es válido";
+                message = INVALID_PRODUCT_ID_MESSAGE;
                 productObj.setAsNotSuccessful(message);
                 return productObj;
             }
             Optional<Product> product = productRepository.findById(id);
             if(!product.isPresent()){
-                message = "No se encontró el producto con ID: " + id;
+                message = NOT_FOUND_PRODUCT_BY_ID_MESSAGE + id;
                 productObj.setAsNotSuccessful(message);
                 return productObj;
             }
@@ -137,10 +149,9 @@ public class ProductApplicationService implements ProductUseCase  {
             message = "El producto: " + product.get().getName() + " con ID: " + id + " fue eliminado exitosamente";
             productObj.setAsSuccessful(message, product.get());
         } catch (Exception e){
-            message = "Error en la ejecucion del proceso: " + e.getMessage();
+            message = DEFAULT_ERROR_MESSAGE + e.getMessage();
             productObj.setAsWithErrors(message);            
         }
         return productObj;
-    }
-    
+    }    
 }
